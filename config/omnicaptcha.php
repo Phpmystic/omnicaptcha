ecb6fbe4fa5488141bec96f203d4603d71f6a6aa
Elmehdi El Fatihi <elmehdielfatihi1111@gmail.com>
2023-07-01 20:41:16 +0100

    add env getter

--- config/omnicaptcha.php
+++ config/omnicaptcha.php
@@ -4,16 +4,16 @@ return [
     "captcha"   => "recaptcha",
     "providers" => [
         "recaptcha" => [
-            "secret_key" => "6LeAH3ojAAAAAH_fW2BuCsWAW1eL2OKADd1ulYRh",
-            "site_key"   => "6LeAH3ojAAAAAIsAC1aOkyzefZidsS8zozuvHfzv",
+            "secret_key" => env('RECAPTCHA_SECRET_KEY', ''),
+            "site_key"   => env('RECAPTCHA_SITE_KEY', '')
         ],
         "hcaptcha" => [
-            "secret_key" => "0xb59AeB0F2adA699D364E2d691Bcde240Cf013D2e",
-            "site_key"   => "80d29d38-4afc-4152-ad2e-30b9e105636a",
+            "secret_key" => env('HCAPTCHA_SECRET_KEY', ''),
+            "site_key"   => env('HCAPTCHA_SITE_KEY', ''),
         ],
         "cloudflare" => [
-            "secret_key" => "0x4AAAAAAAGsvEM-7XGzCpQniw2izTsWtew",
-            "site_key"   => "0x4AAAAAAAGsvK1BmZ1dotG3",
+            "secret_key" => env('CLOUDFLARE_SECRET_KEY', ''),
+            "site_key"   => env('CLOUDFLARE_SITE_KEY', '')
         ],
     ]
 ];
